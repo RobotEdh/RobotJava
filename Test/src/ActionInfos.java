2382db3f97a4335a3f18d727e963168ffc316243
Robotedh <eric.delahoupliere@free.fr>
2013-01-28 09:06:15 +0100

    Get infos

---
+++ Test/src/ActionInfos.java
@@ -0,0 +1,68 @@
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.JTextField;
+
+
+public class ActionInfos extends AbstractAction {
+	
+	private RobotWindow window;
+	private int cmd[] =  {XbeeSend.CMD_INFOS};
+	private int SpeedMotorRight;
+	private int SpeedMotorLeft;
+	private int TickRight;
+	private int TickLeft;
+	private int direction;
+	private int distance;	
+	
+	public ActionInfos(RobotWindow window, String texte){
+		super(texte);
+		
+		this.window = window;
+	}
+	
+	public void actionPerformed(ActionEvent e) { 
+	    System.out.println("Infos");
+	    
+	    try {
+	    	XbeeSend a = new XbeeSend(cmd);
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	    
+		 try {
+			XbeeReceiveInfos b = new XbeeReceiveInfos (
+					 SpeedMotorRight,
+					 SpeedMotorLeft,
+					 TickRight,
+					 TickLeft,
+					 direction,
+					 distance);
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}	
+	} 
+	
+	public int getSpeedMotorRight(){
+		return SpeedMotorRight;
+	}
+	public int getSpeedMotorLeft(){
+		return SpeedMotorLeft;
+	}
+	public int getTickRight(){
+		return TickRight;
+	}
+	public int getTickLeft(){
+		return TickLeft;
+	}
+	public int getdirection(){
+		return direction;
+	}
+	public int getdistance(){
+		return distance;
+	}
+	
+	
+}
